47bd8c337810566cbe1bde58776420271ed5ae4c
Aziz Mevlana Alim <147847410+aziz-mevlana@users.noreply.github.com>
2024-12-22 15:38:58 +0300

    v0.5

--- student_projects/models.py
+++ student_projects/models.py
@@ -4,7 +4,8 @@ class ProjectStudents(models.Model):
     projects_title = models.CharField(max_length=50)
     projects_description = models.CharField(max_length=250)
     projects_topic = models.CharField(max_length=250)
-    #ogrenci id
+    projects_class = models.CharField(max_length=1)
+    
     
     
     def __str__(self):
