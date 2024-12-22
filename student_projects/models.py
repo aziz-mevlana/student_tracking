18da2c9519f9cdc40ef2d6e2056cdec35794f394
Aziz Mevlana Alim <147847410+aziz-mevlana@users.noreply.github.com>
2024-12-22 17:23:47 +0300

    v0.6

--- student_projects/models.py
+++ student_projects/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+
+
 class ProjectStudents(models.Model):
+    CLASS_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, 'G'),
+    ]
+
     projects_title = models.CharField(max_length=50)
     projects_description = models.CharField(max_length=250)
-    projects_topic = models.CharField(max_length=250)
-    projects_class = models.CharField(max_length=1)
+    projects_class = models.IntegerField(choices=CLASS_CHOICES)
+    projects_last_date = models.DateField(null=True)
     
     
     
